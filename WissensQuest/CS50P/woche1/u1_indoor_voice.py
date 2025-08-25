78988ed307259d0b40d128e1a4cde92227e927c4
Horizon-Research-UG <162907990+Horizon-Research-UG@users.noreply.github.com>
2025-08-25 22:02:07 +0200

    bisschen sortiert

---
+++ WissensQuest/CS50P/woche1/u1_indoor_voice.py
@@ -0,0 +1,21 @@
+# Quelle: https://cs50.harvard.edu/python/psets/0/indoor/
+## Vielen herzlichen Dank dafür!!! - sehr sehr starke Leistung :  )
+
+# Übung 1
+
+#Indoor Voice
+print("willkommen bei CS50P - Indoor Voice - die füstersprache")
+#WRITING IN ALL CAPS IS LIKE YELLING.
+impo
+#Best to use your “indoor voice” sometimes, 
+#writing entirely in lowercase.
+
+#In a file called indoor.py, 
+# implement a program in Python 
+# that prompts the user for input and 
+# then outputs that same input in lowercase. 
+# Punctuation and whitespace should be outputted unchanged. 
+# You’re welcome, but not required, 
+# to prompt the user explicitly, 
+# as by passing a str of your own 
+# as an argument to input.
