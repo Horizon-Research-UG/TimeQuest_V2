634bc3eeb683c4a797913157ff19f0321cb0ef09
Horizon-Research-UG <162907990+Horizon-Research-UG@users.noreply.github.com>
2025-08-26 06:53:34 +0200

    Morning py
    Logbuch + erklärung + neue fragen und speichern der antworten in txt

--- Startsequenz.py
+++ Startsequenz.py
@@ -1,12 +1,48 @@
-import werkzeug.abstand as abstand
-import werkzeug.loading as loading
-import time
-import werkzeug.health_bar as health_bar
+#lade bibliotheken
+import werkzeug.abstand as abstand 
+#für abstand
+import werkzeug.loading as loading 
+#für ladeanimation
+import time 
+#für Zeitverzögerung & visuell vereinfachte informationsaufnahme
+import werkzeug.health_bar as health_bar 
+#für health bar
 
-abstand.abstand()
+
+#################
+#einstiegssequenz
+#################
+
+abstand.abstand() 
+#abstand
 print("--- >  Herzlich Willkommen zu TimeQuest!  <---")
-abstand.abstand()
-time.sleep(1)
-loading.loading()
+#drucken
+abstand.abstand() 
+#abstand
+time.sleep(1) 
+#warte 1 sekunde
+loading.loading() 
+#Ladeanimation
 
-health_bar.health_bar()
+health_bar.health_bar() 
+#health bar
+#siehe werkzeug.health_bar
+
+
+###########################
+x = input("Wie kann ich wieder leben? ")
+###########################
+# Ticket 1: X in einer Liste speichern
+
+import csv
+
+def save_value(x, filename="dokumente/werte.csv"):
+    with open(filename, "a", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow([x])
+
+# Beispiel: Wert speichern
+x = input("Gib einen Wert ein: ")
+save_value(x)
+
+#append
